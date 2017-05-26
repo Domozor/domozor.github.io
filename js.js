f27eea48e5d08948863b807d444e317b9cb27ed8
Domozor <domhall14@gmail.com>
2017-05-26 13:56:01 +0100

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var button = document.getElementById("button")
 
 button.onclick = function () {
   var color = document.getElementById("color")
-  color.style.backgroundColor = "#0FF";
+  color.style.backgroundColor = "#08B6FD";
   color.innerHTML = "Hello";
   color.style.height = 200 + "px";
   color.style.color = "black";
