7f1cc451ed03ad285bf8156e70af6ecb1a6491a2
Domozor <domhall14@gmail.com>
2017-05-26 12:35:33 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,9 @@
 var button = document.getElementById("button")
 
 button.onclick = function () {
-  confirm("Hello");
+  var color = document.getElementById("color")
+  color.style.backgroundColor = "#00FF6A";
+  color.innerHTML = "Hello";
+  color.style.height += 100 + "px";
 }
+
