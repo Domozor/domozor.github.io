7236bc906601c78ec237b94d3dbb329b81d04f55
Domozor <domhall14@gmail.com>
2017-05-26 13:51:04 +0100

    Update js.js

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ button.onclick = function () {
   var color = document.getElementById("color")
   color.style.backgroundColor = "#0FF";
   color.innerHTML = "Hello";
-  color.style.height = 300 + "px";
+  color.style.height = 200 + "px";
   color.style.color = "black";
 }
 
